05a95d248758b874a4aff0b3871a57b88e5975af
tolikrss <tolikrss@gmail.com>
2019-06-12 09:13:57 +0300

    update users service

--- src/app/modules/main/services/users.service.ts
+++ src/app/modules/main/services/users.service.ts
@@ -1,19 +1,44 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {QrCodeService} from '../../../services/qr-code.service';
 import {UserModel} from '../models/user.model';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable()
 export class UsersService {
 
-  constructor(
-    private qrCode: QrCodeService,
-  ) { }
+  private _usersList$: BehaviorSubject<UserModel[]> = new BehaviorSubject<UserModel[]>([]);
+  public usersList$: Observable<UserModel[]> = this._usersList$.asObservable();
 
-  addNewUser(userData: UserModel) {
-
+  private get users(): UserModel[] {
+    return this.cloneObj(this._usersList$.value);
   }
 
-  deleteUser(): UserModel {
-    return null;
+  constructor(
+    private qrCode: QrCodeService,
+  ) {
+  }
+
+  addNewUser(userData: UserModel) {
+    userData.id = this.guid();
+
+    this._usersList$.next(this.users.push(userData));
+  }
+
+  deleteUser(id: string | number): void {
+    this._usersList$.next(this.users.filter(u => u.id !== id));
+  }
+
+  guid(): string {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  cloneObj<T extends object>(target: T): T {
+    return JSON.parse(JSON.stringify(target));
   }
 }
